0104534a5aef16a014be13594d7c1be80c03f659
Sourabh Singh <specious0027@gmail.com>
2026-01-04 17:56:02 +0530

    Added keycaps models and textures

--- src/slices/Hero/Scene.tsx
+++ src/slices/Hero/Scene.tsx
@@ -2,6 +2,7 @@
 import { Keyboard } from "@/components/Keyboard";
 import { useControls } from "leva";
 import { Environment, PerspectiveCamera } from "@react-three/drei";
+import { Keycap } from "@/components/Keycap";
 
 export function Scene() {
   const { positionX, positionY, positionZ, rotationX, rotationY, rotationZ } =
@@ -23,10 +24,22 @@ export function Scene() {
       />
       <Environment
         files={["/hdr/blue-studio.hdr"]}
-        environmentIntensity={.05}
+        environmentIntensity={0.1}
       />
+
+      <group>
+        <Keycap position={[0, -0.4, 2.6]} rotation={[0, 2, 3]} texture={0} />
+        <Keycap position={[-1.4, 0, 2.3]} rotation={[3, 2, 1]} texture={1} />
+        <Keycap position={[-1.8, 1, 1.5]} rotation={[1, 0, 3]} texture={2} />
+        <Keycap position={[0.1, 1, 1]} rotation={[0, 4, 2]} texture={3} />
+        <Keycap position={[0.7, 0.9, 1.4]} rotation={[3, 2, 0]} texture={4} />
+        <Keycap position={[1.3, -0.3, 2.3]} rotation={[1, 2, 0]} texture={5} />
+        <Keycap position={[0, 1, 2]} rotation={[2, 2, 3]} texture={6} />
+        <Keycap position={[-0.77, 0.1, 2.8]} rotation={[3, 2, 3]} texture={7} />
+        <Keycap position={[2, 0, 1]} rotation={[0, 0, 3]} texture={8} />
+      </group>
       <spotLight
-        position={[-2, 1.5, 3]}
+        position={[-2, 1.5, 1]}
         intensity={30}
         castShadow
         shadow-bias={-0.0002}
