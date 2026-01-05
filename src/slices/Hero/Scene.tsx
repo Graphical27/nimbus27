41184e5997928b4f351ed33c786878ca947525c1
Sourabh Singh <specious0027@gmail.com>
2026-01-05 13:49:26 +0530

    Bento Box Slice Done

--- src/slices/Hero/Scene.tsx
+++ src/slices/Hero/Scene.tsx
@@ -14,29 +14,42 @@ export function Scene() {
       rotationY: 0.4,
       rotationZ: 0,
     });
+
+  const scalingFactor = window.innerWidth <= 500 ? 0.5 : 1;
   return (
     <group>
       <PerspectiveCamera makeDefault position={[0, 0, 4]} fov={50} />
-      <Keyboard
-        scale={9}
-        position={[0.2, -0.5, 1.9]}
-        rotation={[1.6, 0.4, 0]}
-      />
-      <Environment
-        files={["/hdr/blue-studio.hdr"]}
-        environmentIntensity={0.1}
-      />
 
-      <group>
-        <Keycap position={[0, -0.4, 2.6]} rotation={[0, 2, 3]} texture={0} />
-        <Keycap position={[-1.4, 0, 2.3]} rotation={[3, 2, 1]} texture={1} />
-        <Keycap position={[-1.8, 1, 1.5]} rotation={[1, 0, 3]} texture={2} />
-        <Keycap position={[0.1, 1, 1]} rotation={[0, 4, 2]} texture={3} />
-        <Keycap position={[0.7, 0.9, 1.4]} rotation={[3, 2, 0]} texture={4} />
-        <Keycap position={[1.3, -0.3, 2.3]} rotation={[1, 2, 0]} texture={5} />
-        <Keycap position={[0, 1, 2]} rotation={[2, 2, 3]} texture={6} />
-        <Keycap position={[-0.77, 0.1, 2.8]} rotation={[3, 2, 3]} texture={7} />
-        <Keycap position={[2, 0, 1]} rotation={[0, 0, 3]} texture={8} />
+      <group scale={scalingFactor}>
+        <Keyboard
+          scale={9}
+          position={[0.2, -0.5, 1.9]}
+          rotation={[1.6, 0.4, 0]}
+        />
+        <Environment
+          files={["/hdr/blue-studio.hdr"]}
+          environmentIntensity={0.25}
+        />
+
+        <group>
+          <Keycap position={[0, -0.4, 2.6]} rotation={[0, 2, 3]} texture={0} />
+          <Keycap position={[-1.4, 0, 2.3]} rotation={[3, 2, 1]} texture={1} />
+          <Keycap position={[-1.8, 1, 1.5]} rotation={[1, 0, 3]} texture={2} />
+          <Keycap position={[0.1, 1, 1]} rotation={[0, 4, 2]} texture={3} />
+          <Keycap position={[0.7, 0.9, 1.4]} rotation={[3, 2, 0]} texture={4} />
+          <Keycap
+            position={[1.3, -0.3, 2.3]}
+            rotation={[1, 2, 0]}
+            texture={5}
+          />
+          <Keycap position={[0, 1, 2]} rotation={[2, 2, 3]} texture={6} />
+          <Keycap
+            position={[-0.77, 0.1, 2.8]}
+            rotation={[3, 2, 3]}
+            texture={7}
+          />
+          <Keycap position={[2, 0, 1]} rotation={[0, 0, 3]} texture={8} />
+        </group>
       </group>
       <spotLight
         position={[-2, 1.5, 1]}
